32cac2d95b1ec56e4296f51ca476af2b4d0dce69
Himanshu12866 <manshu010m@gmail.com>
2024-10-06 22:56:29 +0530

    "Changed 'let' to 'var' for myArray declaration."

--- TS/index.ts
+++ TS/index.ts
@@ -1,6 +1,6 @@
 // var varName:string = "Some Name"
 // console.log(varName)
-let myArray:string[] = ["Ajay" , "Seema" , "Pavan" , "Ram" , "Sita"];
+var myArray:string[] = ["Ajay" , "Seema" , "Pavan" , "Ram" , "Sita"];
 for(let i of myArray){
     console.log(i);
 }
